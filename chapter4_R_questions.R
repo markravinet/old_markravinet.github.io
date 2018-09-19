a7af9a62e8aa3c041eb84454d7b6ff325098681a
Mark Ravinet <markravinet@gmail.com>
2018-09-19 16:23:29 +0200

    Uploaded Chapter 4 questions

---
+++ chapter4_R_questions.R
@@ -0,0 +1,55 @@
+## Study questions for Chapter 4 computer lab session ##
+
+# Name:
+# Date:
+
+### Note that the tutorial that will explain most of the answers to these questions is
+### here: https://markravinet.github.io/Chapter4.html
+
+### INSTRUCTIONS ###
+# The easiest way to submit your work is use this R script as a template. You should show
+# your code. You should also show the answers using the hash (#) symbol to comment them
+# out.
+# 
+# When you are finished, you can click, File>Save_as and alter the extension of the script
+# from .R to .txt - this will save it as a text file which you can then upload to Canvas.
+# We recommend you do this last of all, because it will destroy the syntax highlighting in
+# your R code.
+
+### Discussion questions
+
+### 1. What are absolute and relative fitness? What do we mean by marginal fitness?
+
+### 2. What are the key components of the one-locus model of vialbility selection?
+
+### 3. What is the difference between directional selection and balancing selection? 
+
+### R-based questions
+
+### 4. We sample a 315 individuals from a population of snails which have a brown/yellow polymorphism. This is
+###    controlled by a single locus B. B1B1 individuals are brown, so are B1B2 individuals
+###    but B2B2 is yellow. In our sample, 184 snails are B1B1, 60 are B1B2 and 81 are B2B2.
+###    We know from our experimental work that on average, brown individuals have 33 offspring,
+###    whereas yellow individuals have 24. Calculate the following:
+
+###   a. the frequency of the B1 and B2 alleles
+
+###   b. the relative fitnesses 
+
+###   c. the marginal fitness
+
+###   d. the mean population fitness.
+
+### 5. Using R code we learned during the tutorial, simulate selection over 100 generations
+###    for a case where A1A1 has the highest fitness and the relative fitness of the genotypes A1A2 and A2A2 is 0.4. The initial
+###    of the A1 allele is 0.01. Plot the frequency of p over the generations. 
+###    Is A1 dominant, additive or recessive? What can we infer from the plot? 
+
+### N.B You must define the function in your code
+
+
+
+
+
+
+
