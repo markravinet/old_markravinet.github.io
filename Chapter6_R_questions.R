454eb5dd028a50b58b11effe8e49e3c2a6dc1e68
Mark Ravinet <markravinet@gmail.com>
2018-10-03 14:26:12 +0200

    FInalise Chapter 6
    Upload files for Chapter 6

---
+++ Chapter6_R_questions.R
@@ -0,0 +1,51 @@
+## Study questions for Chapter 6 computer lab session ##
+
+# Name:
+# Date:
+
+### Note that the tutorial that will explain most of the answers to these questions is
+### here: https://markravinet.github.io/Chapter5.html
+
+### INSTRUCTIONS ###
+# The easiest way to submit your work is use this R script as a template. You should show
+# your code. You should also show the answers using the hash (#) symbol to comment them
+# out.
+#
+# When you are finished, you can click, File>Save_as and alter the extension of the script
+# from .R to .txt - this will save it as a text file which you can then upload to Canvas.
+# We recommend you do this last of all, because it will destroy the syntax highlighting in
+# your R code.
+
+## 1. Using the iris dataset, visualise the difference in petal width and sepal length
+##    between the species. N.B. You can use a ggplot or base R plot for this
+
+## 2. Again using the iris dataset, calculate the mean petal width and sepal length for each of 
+##    the three species. Once you have done this, do the same for the median. 
+##    NB. There is a way to do all of this in a single command, it is not complusory but 
+##    you are welcome to try if you are feeling adventurous! 
+
+## 3. Use ANOVA to test whether mean petal width and sepal length differs among species. 
+
+## 4. What proportion of the variance explained in petal width and sepal length is due to differences
+##    among species?
+
+## 5. For our QTL analysis, we genotyped our individuals at 334 markers and used an F2 intercross
+##    design. The grandparents of our cross were fixed for AA and BB at each of these informative
+##    markers.
+
+##    a. Explain what we mean by an F2 intercross design
+
+##    b. What are the expected genotype frequences among F2 individuals we used for the actual
+##       QTL experiment?
+
+## 6. Explain what a linkage map is and the units it is expressed in.
+      
+## 7. From the bedbug linkage map, how many markers are there on the first three linkage groups? How 
+##    long are these linkage groups? What is the average spacing among markers for the entire linkage
+##    map?
+      
+## 8. Using R code, count the number of each phenotype among the 71 F2 individuals in the QTL mapping 
+##    mapping experiment
+
+## 9. Use ANOVA to test for an association between bedbug genotype and pesticide resistance phenotype
+##    at a non-qtl marker. What are the biological reasons behind this result?
